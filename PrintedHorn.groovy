1a50e0d616501bd8e01d571dc4e4721601f3da91
lenovo <lenovo@DESKTOP-IA8055S>
2022-08-13 12:21:56 -0700

    init commit
    create task list

--- PrintedHorn.groovy
+++ PrintedHorn.groovy
@@ -1,1 +1,6 @@
-//Your code here
+
+// create larger servo horn geometry
+// modify GeneratedServoSpline?
+// import GeneratedServoSpline as its own geometry (as a negative?)
+// set to generate shape files from release versions
+// release v0.1.1
