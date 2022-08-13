63433f5038e0c607b59db23eae3dae54f4e83fe0
lenovo <lenovo@DESKTOP-IA8055S>
2022-08-13 12:32:09 -0700

    generated pass-thru

--- PrintedHorn.groovy
+++ PrintedHorn.groovy
@@ -4,3 +4,9 @@
 // import GeneratedServoSpline as its own geometry (as a negative?)
 // set to generate shape files from release versions
 // release v0.1.1
+
+// call a script from another library
+def servoHorn = ScriptingEngine.gitScriptRun(
+			"https://github.com/JansenSmith/Luna_Parameterization.git", // git location of the library
+			"GeneratedServoSpline.groovy" , // file to load
+			)
